9e5798141eba94e8f46581ccc5d3becf08a52689
Panagiotis H.M. Issaris <takis@issaris.org>
2014-11-25 00:33:53 +0100

    Hide default stdout output

--- script.sh
+++ script.sh
@@ -2,12 +2,12 @@
 echo "Adding Odoo 8.0 repo"
 echo 'deb http://nightly.openerp.com/8.0/nightly/deb/ ./' > /etc/apt/sources.list.d/odoo.list
 echo "Updating APT cache"
-apt-get update -y -qq
+apt-get update -y -qq >/dev/null
 echo "Installing PostgreSQL"
-apt-get install postgresql -y
+apt-get install postgresql -y >/dev/null
 echo "Starting PostgreSQL"
-service postgresql start
+service postgresql start >/dev/null
 echo "Creating default PostgreSQL database"
 su postgres -s /bin/bash -c "createdb mycompany"
 echo "Installing Odoo"
-apt-get install odoo -f -y --force-yes
+apt-get install odoo -f -y --force-yes >/dev/null
