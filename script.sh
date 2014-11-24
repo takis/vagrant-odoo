4f62e5811fc3e1d03489ada34474896a20c6b703
Panagiotis H.M. Issaris <takis@issaris.org>
2014-11-25 00:25:11 +0100

    Show what the script is doing on stdout

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+echo "Adding Odoo 8.0 repo"
 echo 'deb http://nightly.openerp.com/8.0/nightly/deb/ ./' > /etc/apt/sources.list.d/odoo.list
+echo "Updating APT cache"
 apt-get update -y -qq
+echo "Installing PostgreSQL"
 apt-get install postgresql -y
+echo "Starting PostgreSQL"
 service postgresql start
+echo "Creating default PostgreSQL database"
 su postgres -s /bin/bash -c "createdb mycompany"
+echo "Installing Odoo"
 apt-get install odoo -f -y --force-yes
